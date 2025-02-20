3a26cd36e99fd8f9cef375697e76ebf67c17eba9
Joseph Collado <sourpina@gmail.com>
2025-02-20 14:06:49 -0500

    Added Dice class with features

---
+++ Dice.cs
@@ -0,0 +1,43 @@
+﻿/* Joseph Collado
+
+UML
+Class: Dice 
+
+Fields:
+- numSides : int
+
+Properties:
++ Sides : int
+
+Methods:
++ Dice(sides: int)
++ Roll() : int
+
+*/
+
+class Dice
+{
+    private int numSides;
+
+    public int Sides 
+    { 
+        get => numSides; 
+        private set
+        {
+            // Ensures the dice has at least one face
+            if (value < 1)
+                value = 1;
+            
+            numSides = value;
+        } 
+    }
+
+    public Dice(int sides = 6) => Sides = sides;
+
+    // Returns a random integer between 1 and the number of sides, inclusive
+    public int Roll()
+    {
+        Random random = new Random();
+        return random.Next(0, Sides) + 1;
+    }
+}
